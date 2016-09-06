a06782518db7877a345969d171823c37f80745dc
Jason Stewart <jstewart-amd@users.noreply.github.com>
2016-09-06 15:37:23 -0400

    Update to latest premake

--- premake/amd_premake_util.lua
+++ premake/amd_premake_util.lua
@@ -5,11 +5,21 @@
 -- Strip "vs" from this string to make a suffix for solution and project files.
 _AMD_VS_SUFFIX = "_" .. string.gsub(_ACTION, "vs", "")
 
+-- _ACTION is a premake global variable and for our usage will be vs2012, vs2013, etc.
+-- Make an allcaps version. We use this for some directory names.
+_AMD_ACTION_ALL_CAPS = string.upper(_ACTION)
+
 -- Specify build output directory structure here: e.g. Desktop_2012\x64\DLL_Debug
 _AMD_SAMPLE_DIR_LAYOUT  = "Desktop%{_AMD_VS_SUFFIX}/%{cfg.platform}/%{cfg.buildcfg}"
+_AMD_LIBRARY_DIR_LAYOUT = "%{_AMD_ACTION_ALL_CAPS}/%{cfg.platform}/%{cfg.buildcfg}"
+
+-- Some projects have a "minimal dependencies" build. Give them a different output
+-- directory structure to avoid collisions with the full-dependencies version.
+_AMD_SAMPLE_DIR_LAYOUT_MINIMAL  = "Desktop%{_AMD_VS_SUFFIX}/minimal/%{cfg.platform}/%{cfg.buildcfg}"
+_AMD_LIBRARY_DIR_LAYOUT_MINIMAL = "%{_AMD_ACTION_ALL_CAPS}/minimal/%{cfg.platform}/%{cfg.buildcfg}"
 
 -- Specify WindowsTargetPlatformVersion here for VS2015
-_AMD_WIN_SDK_VERSION = "10.0.10240.0"
+_AMD_WIN_SDK_VERSION = "8.1"
 
 -- command lines for Visual Studio build events
 _AMD_COPY_WIN_8_0_SDK_REDIST_TO_BIN = "if not exist \"..\\bin\\d3dcompiler_46.dll\" if exist \"$(ProgramFiles)\\Windows Kits\\8.0\\Redist\\D3D\\x64\\d3dcompiler_46.dll\" xcopy \"$(ProgramFiles)\\Windows Kits\\8.0\\Redist\\D3D\\x64\\d3dcompiler_46.dll\" \"..\\bin\" /H /R /Y > nul"
